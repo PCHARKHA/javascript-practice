c1345cd6a7c7553673db6980e1478695f45f6c2c
Pranjal Charkha <pranjal.charkha37@gmail.com>
2026-07-15 14:01:01 +0530

    refactor: reorganize JavaScript folder structure and DOM modules

---
+++ DOM/02-Traversal/dataset.js
@@ -0,0 +1,7 @@
+const student = document.getElementById("student");
+
+console.log(student.dataset);
+
+console.log(student.dataset.name);
+console.log(student.dataset.course);
+console.log(student.dataset.batch);
